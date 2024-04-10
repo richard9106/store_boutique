89b1f2e9f504d33fc914422c5c8f8916397bee60
Ricardo marles <andresmarles9106@gmail.com>
2024-04-10 14:18:20 +0200

    updating shopping bag template

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
@@ -7,6 +9,18 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
 
     if total > settings.FREE_DELIERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIERY_PERCENTAGE)
@@ -20,9 +34,9 @@ def bag_contents(request):
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count,
-        'delivery': delivery,
-        'free_delivery_delta': free_delivery_delta,
+        'produc t_count': product_count,
+        'deliver y': delivery,
+        'free_del    ivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIERY_THRESHOLD,
         'grand_total': grand_total,
     }
