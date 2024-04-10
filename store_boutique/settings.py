2bab27c5abf592ebc94b110fe506f20f33113ba0
Ricardo marles <andresmarles9106@gmail.com>
2024-04-10 12:04:47 +0200

    Adding bag context and delivery logic

--- store_boutique/settings.py
+++ store_boutique/settings.py
@@ -85,6 +85,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -173,7 +174,13 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+FREE_DELIERY_THRESHOLD = 50
+STANDARD_DELIERY_PERCENTAGE = 10
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
