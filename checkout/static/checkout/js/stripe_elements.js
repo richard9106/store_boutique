f485da2d0fb81dda3c17b26dd826bd3b9f96d7f1
Ricardo marles <andresmarles9106@gmail.com>
2024-04-18 12:09:22 +0200

    changin stripe lenguaje to english

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -8,7 +8,7 @@
 
 var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripe = Stripe(stripe_public_key, {locale: 'en'});
 var elements = stripe.elements();
 var style = {
     base: {
