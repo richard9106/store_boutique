633867c56647b49306eb3dfa2ec4c0536dbfced4
Ricardo marles <andresmarles9106@gmail.com>
2024-04-22 12:16:25 +0200

    adding spinner animation checkout page

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -53,6 +53,9 @@ form.addEventListener("submit", function (ev) {
   ev.preventDefault();
   card.update({ disabled: true });
   $("#submit-button").attr("disabled", true);
+  $("#payment-form").fadeToggle(100);
+  $("#loading-overlay").fadeToggle(100);
+
   stripe.confirmCardPayment(clientSecret, {
     payment_method:{
       type: 'card',
@@ -67,6 +70,9 @@ form.addEventListener("submit", function (ev) {
                 </span>
                 <span>${result.error.message}</span>`;
         $(errorDiv).html(html);
+        $("#payment-form").fadeToggle(100);
+        $("#loading-overlay").fadeToggle(100);
+
         card.update({ disabled: false });
         $("#submit-button").attr("disabled", false);
       } else {
