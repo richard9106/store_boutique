8417d766b01d5f0671423e8f47410d42161d3367
Richard marles <andresmarles9106@gmail.com>
2024-04-25 09:35:33 +0000

    adding cach data from webhook in strjipewh_handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -18,7 +18,10 @@ class OrderAdmin(admin.ModelAdmin):
                        'date',
                        'delivery_cost',
                        'order_total',
-                       'grand_total',)
+                       'grand_total',
+                       'original_bag',
+                       'stripe_pid',
+                       )
 
     fields = ('order_number',
               'date',
@@ -34,6 +37,8 @@ class OrderAdmin(admin.ModelAdmin):
               'delivery_cost',
               'order_total',
               'grand_total',
+              'original_bag',
+              'stripe_pid',
               )
 
     list_display = ('order_number',
