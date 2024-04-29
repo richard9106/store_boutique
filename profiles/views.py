1eda18cfc56271274570c501c22bce8816b6c35e
Richard marles <andresmarles9106@gmail.com>
2024-04-29 07:15:50 +0000

    adding profile form to update profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import UserProfile
+from .forms import UserProfileForm
 
 # Create your views here.
 def profile(request):
     """ view fro profile page"""
-    profile = get_object_or_404(UserProfile, user=request.user)    
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid:
+            form.save()
+            messages.success(request, "Profile update successfully")
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = "profile/profile.html"
     context = {
-        'profile':profile,
+        'form':form,
+        'orders':orders,
+        'on_profile_page':True,
         }
 
     return render(request, template, context)
