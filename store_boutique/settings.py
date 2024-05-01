2eec0a6f10e7af92cd67777574264503c7fc6377
Richard marles <andresmarles9106@gmail.com>
2024-05-01 10:52:18 +0000

    changing django secret key

--- store_boutique/settings.py
+++ store_boutique/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)mw)hqmtimbc#%m!d1!db-)rle=)=zz1_-&u@2q*tq)ay-y*#l'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', '.gitpod.io']
 
