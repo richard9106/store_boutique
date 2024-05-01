d4a8afb6d98032f004d03f4f7486c40d4d824cea
Richard marles <andresmarles9106@gmail.com>
2024-05-01 07:37:15 +0000

    Admin delete prodcut functionality

--- products/views.py
+++ products/views.py
@@ -73,9 +73,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfuly added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please check your form')
     else:
@@ -109,3 +109,10 @@ def edit_product(request, product_id):
         'product':product,
     }
     return render(request, template, context)
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
